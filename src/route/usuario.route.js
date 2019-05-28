b9a3391acac22300cd7fd5afd5cb98079f2d282b
Manassés Júlio <jmanasses1024@hotmail.com>
2019-05-28 19:17:50 -0300

    Padronização das routs
    Apenas modificações nas routas para ficarem um pouco mais padronizadas.

--- src/route/usuario.route.js
+++ src/route/usuario.route.js
@@ -1,15 +1,15 @@
 module.exports = function(router) {
 	const Usuario = require("../controller/usuario.controller.js");
 
-	router.post("/api/usuario", Usuario.create);
+	router.post("/create", Usuario.create);
 
-	router.get("usuario/:login/:senha", Usuario.findAll);
+	router.get("/:login/:senha", Usuario.findAll);
 
 	router.post("/login", Usuario.login);
 
 	router.put('/disable/:id', Usuario.disable); 
 
-	router.put("/api/usuario/:idUsuario", Usuario.update);
+	router.put("/:idUsuario", Usuario.update);
 
 	return router;
 };
