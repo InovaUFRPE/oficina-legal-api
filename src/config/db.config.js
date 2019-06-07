7c88fad0c0701a38cc38ec97ef1ea793cb93ca32
Henrique Cesar <hcesarjs@gmail.com>
2019-06-07 18:59:13 -0300

    Adição de rotas para WEB

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -23,32 +23,35 @@ db.mecanico = require("../model/Mecanico.js")(sequelize, Sequelize);
 db.oficina = require("../model/Oficina.js")(sequelize, Sequelize);
 db.laudo = require("../model/Laudo.js")(sequelize, Sequelize);
 db.os = require("../model/os.js")(sequelize, Sequelize);
-db.veiculo = require('../model/Veiculo')(sequelize, Sequelize);
-db.gestor = require('../model/Gestor.js')(sequelize, Sequelize);
-db.agendamento = require('../model/Agendamento.js')(sequelize, Sequelize);
+db.veiculo = require("../model/Veiculo")(sequelize, Sequelize);
+db.gestor = require("../model/Gestor.js")(sequelize, Sequelize);
+db.agendamento = require("../model/Agendamento.js")(sequelize, Sequelize);
 
+
+db.oficina.hasOne(db.gestor,{foreignKey: "idOficina"});
+db.gestor.belongsTo(db.oficina,{foreignKey: "idOficina"});
+
+db.agendamento.belongsTo(db.veiculo,{foreignKey:"idVeiculo"});
+db.veiculo.hasOne(db.agendamento,{foreignKey:"id"});
+
+db.agendamento.belongsTo(db.oficina,{foreignKey:"idVeiculo"});
+db.oficina.hasOne(db.veiculo,{foreignKey:"id"});
+
+db.cliente.hasOne(db.usuario, {foreignKey: "idUsuario"});
+db.mecanico.hasOne(db.usuario, {foreignKey: "idUsuario"});
+db.adm.hasOne(db.usuario, {foreignKey: "idUsuario"});
+db.gestor.hasOne(db.usuario, {foreignKey: "idUsuario"});
+
+db.veiculo.belongsTo(db.cliente, {foreignKey: "idCliente"});
+
+db.mecanico.belongsTo(db.oficina, { foreignKey: "id" });
 db.mecanicoOS.hasOne(db.os, { foreignKey: "id" });
 db.mecanicoOS.hasOne(db.mecanico, { foreignKey: "id" });
 db.certificado.hasOne(db.mecanico, { foreignKey: "id" });
-db.os.hasOne(db.laudo, { foreignKey: "id" });
-db.os.hasOne(db.oficina, { foreignKey: "id" });
-//db.laudo.hasOne(db.veiculo, { foreignKey: "idVeiculo" });
-//db.mecanico.hasOne(db.usuario, { foreignKey: "idUsuario" });
-db.mecanico.hasOne(db.oficina, { foreignKey: "id" });
-db.usuario.hasOne(db.cliente, { foreignKey: "idUsuario" });
-db.usuario.hasOne(db.mecanico, {foreignKey:"idUsuario"});
-db.cliente.belongsTo(db.usuario, { foreignKey: "idUsuario" });
 
-db.cliente.hasOne(db.veiculo, {foreignKey: "idCliente"})
-db.veiculo.belongsTo(db.cliente,{foreignKey: "idCliente"})
+db.laudo.belongsTo(db.veiculo, {foreignKey: "idVeiculo"});
 
-db.oficina.hasOne(db.gestor,{foreignKey: 'idOficina'})
-db.gestor.belongsTo(db.oficina,{foreignKey: 'idOficina'}) 
-
-db.agendamento.belongsTo(db.veiculo,{foreignKey:'idVeiculo'})
-db.veiculo.hasOne(db.agendamento,{foreignKey:"id"})
-
-db.agendamento.belongsTo(db.oficina,{foreignKey:'idVeiculo'})
-db.oficina.hasOne(db.veiculo,{foreignKey:"id"})
+db.os.belongsTo(db.laudo, { foreignKey: "id" });
+db.os.belongsTo(db.oficina, { foreignKey: "id" });
 
 module.exports = db;
