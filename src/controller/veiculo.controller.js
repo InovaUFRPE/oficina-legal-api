f7d8f1450b2199090c59048b18496f3f7eca173f
Henrique Cesar <hcesarjs@gmail.com>
2019-06-08 21:52:21 -0300

    Rota e função para buscar veículos de um cliente

--- src/controller/veiculo.controller.js
+++ src/controller/veiculo.controller.js
@@ -20,3 +20,14 @@ exports.findById = async (req, res) => {
     }
     return res.status(404).send({alert: "Veículo não encontrado."});
 };
+exports.findAllByCliente = async (req, res) => {
+    try {
+        const veiculos = await Veiculos.findAll({
+            where: { idCliente: req.params.idCliente},
+            attributes: ['id','modelo','ano','renavam','placa']
+        });
+        res.status(200).send(veiculos)
+    } catch (err) {
+		res.status(400).send(err);
+	};
+}
