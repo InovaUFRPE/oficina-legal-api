4404ac5e6663f5c9ff4e0cb99b6cf81a947a6c90
Italo Sousa <italusousa@gmail.com>
2019-06-10 22:58:20 -0300

    correção de lint

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const http = require("http");
+const dotenv = require("dotenv-safe");
+
+if (!process.env.NODE_ENV) {
+  dotenv.load();
+}
+
+console.log('>>', process.env.PORT);
 
 const app = require("./server");
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
+
 
 server.on("error", err => {
   console.log('>>', err.message);
